5266c9890f59b55ea77981202fde64e75242c054
Mark Andrew <jftmain@gmail.com>
2024-05-08 19:45:53 +0300

    nice logo

--- src/components/LogoComponent.tsx
+++ src/components/LogoComponent.tsx
@@ -3,13 +3,26 @@ export const LogoComponent = () => {
     <div className="logo-grid-column">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1081 217"
+        viewBox="0 0 1080 216"
         width="100%"
         className="item-grid-svg"
+        preserveAspectRatio="xMidYMid slice"
       >
-        <path
-          d="M780,468h0a36,36,0,0,1-36,36H456a36,36,0,0,0-36,36v72a36,36,0,0,0,36,36h936a36,36,0,0,0,36-36h0a36,36,0,0,1,36-36h0a36,36,0,0,0,36-36V468a36,36,0,0,0-36-36H816A36,36,0,0,0,780,468Z"
-          transform="translate(-419.5 -431.5)"
+        <defs>
+          <clipPath id="clip">
+            <path
+              d="M780,468h0a36,36,0,0,1-36,36H456a36,36,0,0,0-36,36v72a36,36,0,0,0,36,36h936a36,36,0,0,0,36-36h0a36,36,0,0,1,36-36h0a36,36,0,0,0,36-36V468a36,36,0,0,0-36-36H816A36,36,0,0,0,780,468Z"
+              transform="translate(-420 -432)"
+            />
+          </clipPath>
+        </defs>
+        <image
+          href="/img/minimal-923194_1920.jpg"
+          width="1080"
+          x="0"
+          y="-744"
+          clip-path="url(#clip)"
+          preserveAspectRatio="xMidYMid slice"
         />
       </svg>
 
