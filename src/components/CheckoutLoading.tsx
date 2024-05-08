9adf81e33c84ad71cdf1661dbf46b5dadf2fa89b
Mark Andrew <jftmain@gmail.com>
2024-05-08 06:34:44 +0300

    loading spinner

---
+++ src/components/CheckoutLoading.tsx
@@ -0,0 +1,10 @@
+export const CheckoutLoading = () => {
+  return (
+    <div className="spinner">
+      Loading
+      <div className="spinner-sector spinner-sector-red"></div>
+      <div className="spinner-sector spinner-sector-blue"></div>
+      <div className="spinner-sector spinner-sector-green"></div>
+    </div>
+  );
+};
