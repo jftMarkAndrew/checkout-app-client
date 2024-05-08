b56dccab0b93b1584647455d9283c3617cb6e3ae
Mark Andrew <jftmain@gmail.com>
2024-05-08 18:54:07 +0300

    preparation

---
+++ src/components/StoreComponent.tsx
@@ -0,0 +1,9 @@
+import { ItemComponent } from "./ItemComponent";
+
+export const StoreComponent = () => {
+  return (
+    <div className="store-container">
+      <ItemComponent />
+    </div>
+  );
+};
