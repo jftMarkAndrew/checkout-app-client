65518f898727b8c74b31d1dcc2a2241d20bb4ecf
Mark Andrew <jftmain@gmail.com>
2024-05-08 19:54:01 +0300

    item preparation

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -7,9 +7,20 @@ export const ItemComponent = () => {
         width="100%"
         className="item-grid-svg"
       >
-        <path
-          d="M780,396V684a36,36,0,0,0,36,36h216a36,36,0,0,0,36-36h0a36,36,0,0,1,36-36h0a36,36,0,0,0,36-36V396a36,36,0,0,0-36-36H816A36,36,0,0,0,780,396Z"
-          transform="translate(-780 -360)"
+        <defs>
+          <clipPath id="clipShape">
+            <path
+              d="M780,396V684a36,36,0,0,0,36,36h216a36,36,0,0,0,36-36h0a36,36,0,0,1,36-36h0a36,36,0,0,0,36-36V396a36,36,0,0,0-36-36H816A36,36,0,0,0,780,396Z"
+              transform="translate(-780 -360)"
+            />
+          </clipPath>
+        </defs>
+        <image
+          href="/public/img/01461f32-ba2b-45e8-af80-21b37fddb418.png"
+          width="100%"
+          height="100%"
+          clipPath="url(#clipShape)"
+          preserveAspectRatio="xMidYMid slice"
         />
       </svg>
       <div className="item-grid-bottom-right-corner"></div>
