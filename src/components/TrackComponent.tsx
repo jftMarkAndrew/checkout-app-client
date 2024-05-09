dde310b041b877c6088fc6610a84a0e5754c7ec8
Mark Andrew <jftmain@gmail.com>
2024-05-09 11:46:47 +0300

    order details preparation

--- src/components/TrackComponent.tsx
+++ src/components/TrackComponent.tsx
@@ -1,8 +1,38 @@
 import { useParams } from "react-router-dom";
 import { LogoComponent } from "./LogoComponent";
+import { useEffect, useState } from "react";
+import { OrderDetails } from "../interfaces/orderDetails";
 
 export const TrackComponent = () => {
   const { orderId } = useParams();
+  const [orderDetails, setOrderDetails] = useState<OrderDetails | null>(null);
+
+  useEffect(() => {
+    if (orderId) {
+      fetchOrderDetails(orderId)
+        .then((details) => {
+          setOrderDetails(details);
+        })
+        .catch((error) => {
+          console.error("Failed to fetch order details:", error);
+        });
+    }
+  }, [orderId]);
+
+  async function fetchOrderDetails(orderId: string) {
+    try {
+      const response = await fetch(`http://localhost:3000/checkout/${orderId}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch order details");
+      }
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch order details:", error);
+      throw error;
+    }
+  }
 
   return (
     <div>
@@ -15,7 +45,29 @@ export const TrackComponent = () => {
         </p>
         {orderId && <div>Tracking Order ID: {orderId}</div>}
       </div>
-      {orderId && <div className="tracking-table">table with result</div>}
+      {orderId && (
+        <div className="tracking-table">
+          <h1>Order Tracking</h1>
+          <p>Order ID: {orderId}</p>
+          <p>Data: {}</p>
+          {orderDetails && (
+            <div>
+              <h1>Order Tracking</h1>
+              <p>Order ID: {orderDetails.authorizationId || "N/A"}</p>
+              <p>Status: {orderDetails.authorizationStatus || "N/A"}</p>
+              <p>
+                Amount: {orderDetails.amount || "0"}{" "}
+                {orderDetails.currency || "N/A"}
+              </p>
+              <p>Merchant ID: {orderDetails.merchantId || "N/A"}</p>
+              <p>Transaction Date: {orderDetails.createdAt || "N/A"}</p>
+              {/* Display additional details as needed */}
+              <p>Data: {JSON.stringify(orderDetails)}</p>{" "}
+              {/* Show the whole object as a string for debugging */}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/interfaces/orderDetails.ts
@@ -0,0 +1,100 @@
+export interface DeviceDetails {
+  id: string;
+  deviceType: string;
+  deviceOs: string;
+  browser: string;
+  ipAddress: string;
+  acceptHeader: string;
+  colorDepth: string;
+  javaEnabled: string;
+  javaScriptEnabled: string;
+  browserLanguage: string;
+  screenHeight: number;
+  screenWidth: number;
+  timeZone: string;
+  userAgent: string;
+}
+
+export interface Consumer {
+  consumerId: string;
+  phone: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  email: string;
+  shippingAddress: {
+    city: string;
+    country: string;
+    line2: string;
+    line1: string;
+    postalCode: string;
+  };
+}
+
+export interface PaymentOption {
+  paymentOptionId: string;
+  paymentOptionType: string;
+  paymentFlow: string;
+  brand: string;
+  expirationYear: string;
+  expirationMonth: string;
+  bin: string;
+  issuerCountry: string;
+  nameOnCard: string;
+  last4digits: string;
+}
+
+export interface Transaction {
+  id: string;
+  authorizationId: string;
+  paymentOptionId: string;
+  paymentOptionType: string;
+  merchantId: string;
+  sellerId: string;
+  country: string;
+  issuerCountry: string;
+  paymentMethod: string;
+  brand: string;
+  transactionType: string;
+  type: string;
+  currency: string;
+  amount: number;
+  orderid: string;
+  transactionResult: string;
+  processorTransactionId: string;
+  last4digits: string;
+  program: string;
+  fundsFlow: string;
+  items: [];
+  reconciled: boolean;
+  reconciliationStatus: string;
+  correlationId: string;
+  createdAt: string;
+  updatedAt: string;
+  identificationReference: string;
+  processorAuthCode: string;
+  processorId: string;
+}
+
+export interface OrderDetails {
+  authorizationId: string;
+  authorizationStatus: string;
+  merchantId: string;
+  currency: string;
+  amount: number;
+  orderId: string;
+  deviceDetails: DeviceDetails;
+  items: [];
+  threeDversion: string;
+  merchantName: string;
+  consumer: Consumer;
+  paymentOption: PaymentOption;
+  authentication: {
+    isLiabilityShift: string;
+    threeDversion: string;
+  };
+  transactions: Transaction[];
+  createdAt: string;
+  updatedAt: string;
+  pos: null | string;
+}
