bf768f4200dfbdd19ea9262eb89db7a6522e8bdb
Mark Andrew <jftmain@gmail.com>
2024-05-09 13:14:55 +0300

    conditional error msgs

--- src/components/ErrorMessageComponent.tsx
+++ src/components/ErrorMessageComponent.tsx
@@ -1,5 +1,19 @@
-export const ErrorMessageComponent = () => {
-  return (
-    <p className="text-error">Something went wrong. Give it another chance.</p>
-  );
+interface ErrorMessageComponentProps {
+  errorType?: "LS" | "IF";
+}
+
+export const ErrorMessageComponent: React.FC<ErrorMessageComponentProps> = ({
+  errorType,
+}) => {
+  let message = "Something went wrong. Give it another chance!";
+
+  if (errorType === "LS") {
+    message =
+      "Your card is on the lost / stolen list. Please contact your bank.";
+  } else if (errorType === "IF") {
+    message =
+      "Not enough funds. Please check your account balance and try again later.";
+  }
+
+  return <p className="text-error">{message}</p>;
 };
