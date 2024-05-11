e304b33d4c2281d95d0d72a7a9b6213d0cf7565c
Mark Andrew <jftmain@gmail.com>
2024-05-11 20:08:37 +0300

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ContainerComponent } from "./components/ContainerComponent";
 import { TrackComponent } from "./components/TrackComponent";
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div>
-        {/* <nav>
-          <Link to="/">Store</Link> | <Link to="/tracking">Orders</Link>
-        </nav> */}
-        <Routes>
-          <Route path="/" element={<ContainerComponent />} />
-          <Route path="/tracking" element={<TrackComponent />} />
-          <Route path="/tracking/:orderId" element={<TrackComponent />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<ContainerComponent />} />
+        <Route path="/tracking" element={<TrackComponent />} />
+        <Route path="/tracking/:orderId" element={<TrackComponent />} />
+      </Routes>
     </BrowserRouter>
   );
 };
