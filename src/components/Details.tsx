7202490a9cf744b728956385d1177e0435a499b8
Mark Andrew <jftmain@gmail.com>
2024-05-14 22:47:46 +0300

    details cart

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,6 +1,9 @@
-import validator from "validator";
+import { useState } from "react";
 import { CartItem } from "../interfaces/CartItem";
 import { Currency, currencyCodes } from "../consts/currencyCodes";
+import validator from "validator";
+import { MdLocalGroceryStore } from "react-icons/md";
+import { IoCloseOutline } from "react-icons/io5";
 
 interface DetailsProps {
   cart: CartItem[];
@@ -17,6 +20,8 @@ export const Details: React.FC<DetailsProps> = ({
   onEmailChange,
   onContinue,
 }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   let total = 0;
 
   currency === Currency.GBP
@@ -46,26 +51,46 @@ export const Details: React.FC<DetailsProps> = ({
     return validator.isEmail(email);
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (isValidEmail(email)) {
+      if (event.key === "Enter") {
+        onContinue();
+      }
+    }
+  };
+
   return (
-    <div className="details-container">
-      <h1 className="text-shadow">
-        {currency}
-        {total.toFixed(0)}
-      </h1>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => onEmailChange(e.target.value)}
-        placeholder="Enter your email"
-        className="details-email-input"
-      />
-      <button
-        className="details-email-button"
-        onClick={onContinue}
-        disabled={!isValidEmail(email)}
-      >
-        Continue
-      </button>
-    </div>
+    <>
+      {!showDetails && (
+        <div className="open-details-btn">
+          <MdLocalGroceryStore onClick={() => setShowDetails(true)} className="open-details-cart" />
+        </div>
+      )}
+
+      {showDetails && (
+        <div className="details-container">
+          <IoCloseOutline size="2em" onClick={() => setShowDetails(false)} />
+          <h1 className="text-shadow">
+            {currency}
+            {total.toFixed(0)}
+          </h1>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => onEmailChange(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Enter your email"
+            className="details-email-input"
+          />
+          <button
+            className="details-email-button"
+            onClick={onContinue}
+            disabled={!isValidEmail(email)}
+          >
+            Continue
+          </button>
+        </div>
+      )}
+    </>
   );
 };
