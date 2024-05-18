c2b77296bd0d8f9c3cf7799678c9fa8f2f7dbcf5
Mark Andrew <jftmain@gmail.com>
2024-05-18 17:39:30 +0300

    with state management

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -3,19 +3,16 @@ import React from "react";
 import { Item } from "./Item";
 import { Product } from "../interfaces/Product";
 import { CartItem } from "../interfaces/CartItem";
-import { CurrencySymbol } from "../interfaces/Currency";
 
 interface StoreProps {
   products: Product[];
   cart: CartItem[];
-  currency: CurrencySymbol;
   onAddToCart: (product: Product) => void;
 }
 
 export const Store: React.FC<StoreProps> = ({
   products,
   cart,
-  currency,
   onAddToCart,
 }) => {
   return (
@@ -32,7 +29,6 @@ export const Store: React.FC<StoreProps> = ({
             imageUrl={product.imageUrl}
             productName={product.name}
             productPriceGBP={product.priceGBP.toFixed(0)}
-            currency={currency}
             quantity={quantity}
             onAddItem={() => onAddToCart(product)}
           />
