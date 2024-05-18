484f9e69d98773876dc78adfdd4d06e1c8258317
Mark Andrew <jftmain@gmail.com>
2024-05-18 23:34:40 +0300

    new providers

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,25 @@ import "./index.css";
 import { App } from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ItemProvider } from "./context/ItemContext";
 import { CurrencyProvider } from "./context/CurrencyContext";
 import { CountryProvider } from "./context/CountryContext";
+import { CartProvider } from "./context/CartContext";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <CurrencyProvider>
-        <CountryProvider>
-          <App />
-        </CountryProvider>
-      </CurrencyProvider>
+      <ItemProvider>
+        <CurrencyProvider>
+          <CountryProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CountryProvider>
+        </CurrencyProvider>
+      </ItemProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
