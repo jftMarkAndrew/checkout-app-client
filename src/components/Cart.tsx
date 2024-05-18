c644d80d844a92e1161a03be0891e2610f68c766
Mark Andrew <jftmain@gmail.com>
2024-05-18 23:33:40 +0300

    cart prototype

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,3 +1,36 @@
-export const Cart = () => {
-  return <div>Cart</div>;
+import { useCartContext } from "../context/CartContext";
+import { useCurrencyContext } from "../context/CurrencyContext";
+import { currencySymbols } from "../consts/currencyCodes";
+
+export const Cart: React.FC = () => {
+  const { cart, removeFromCart, clearCart } = useCartContext();
+  const { currency } = useCurrencyContext();
+
+  return (
+    <div className="cart-container">
+      <h2>Your Cart</h2>
+      {cart.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <>
+          <ul>
+            {cart.map((cartItem) => (
+              <li key={cartItem.product.id}>
+                <span>
+                  {cartItem.product.name} -{" "}
+                  {currency ? currencySymbols[currency] : ""}
+                  {cartItem.product.defaultAmount.toFixed(2)}
+                </span>
+                <span>Quantity: {cartItem.quantity}</span>
+                <button onClick={() => removeFromCart(cartItem.product.id)}>
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={clearCart}>Clear Cart</button>
+        </>
+      )}
+    </div>
+  );
 };
