74ab3394b7c0135ee13b60cd0267ad4ba2677d51
Mark Andrew <jftmain@gmail.com>
2024-05-19 02:39:21 +0300

    details to cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import validator from "validator";
 import { useCartContext } from "../context/CartContext";
 import { useCurrencyContext } from "../context/CurrencyContext";
 import { currencySymbols, currencyValues } from "../consts/currencyCodes";
 
 export const Cart: React.FC = () => {
-  const { cart, removeFromCart, clearCart } = useCartContext();
+  const { cart, addToCart, removeFromCart, clearCart } = useCartContext();
   const { currency } = useCurrencyContext();
+  const [email, setEmail] = useState("");
+
+  const isValidEmail = (email: string): boolean => {
+    return validator.isEmail(email);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (isValidEmail(email) && event.key === "Enter") {
+      console.log("Enter");
+    }
+  };
 
   const getTotalPrice = () => {
     return cart
@@ -12,12 +25,12 @@ export const Cart: React.FC = () => {
         const price = cartItem.product.defaultAmount * currencyValues[currency];
         return total + price * cartItem.quantity;
       }, 0)
-      .toFixed(2);
+      .toFixed(0);
   };
 
   return (
-    <div className="cart-container">
-      <h2>Your Cart</h2>
+    <div className="tracking-container">
+      <h1>Cart</h1>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
@@ -30,22 +43,37 @@ export const Cart: React.FC = () => {
                   {currency ? currencySymbols[currency] : ""}
                   {(
                     cartItem.product.defaultAmount * currencyValues[currency]
-                  ).toFixed(2)}
+                  ).toFixed(0)}
                 </span>
                 <span>Quantity: {cartItem.quantity}</span>
                 <button onClick={() => removeFromCart(cartItem.product.id)}>
-                  Remove
+                  −
                 </button>
+                <button onClick={() => addToCart(cartItem.product)}>+</button>
               </li>
             ))}
           </ul>
-          <div>
-            <strong>
-              Total: {currency ? currencySymbols[currency] : ""}
-              {getTotalPrice()}
-            </strong>
-          </div>
           <button onClick={clearCart}>Clear Cart</button>
+
+          <h3 className="text-shadow">
+            Total Amount: {currency}
+            {getTotalPrice()}
+          </h3>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Enter your email"
+            className="details-email-input"
+          />
+          <button
+            className="details-email-button"
+            onClick={() => console.log(email, getTotalPrice())}
+            disabled={!isValidEmail(email)}
+          >
+            Continue
+          </button>
         </>
       )}
     </div>
