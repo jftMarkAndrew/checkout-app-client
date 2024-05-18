b5a4bdc2b78d96c0aed7c7fa145d0c8bccb23b7c
Mark Andrew <jftmain@gmail.com>
2024-05-18 16:11:45 +0300

    providers

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import "./index.css";
 import { App } from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CurrencyProvider } from "./context/CurrencyContext";
+import { CountryProvider } from "./context/CountryContext";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <CurrencyProvider>
+        <CountryProvider>
+          <App />
+        </CountryProvider>
+      </CurrencyProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
